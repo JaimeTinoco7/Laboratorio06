f4796e1e49d720ff747814cd0892bc9f2aab7a4a
JaimeTinoco7 <142696839+JaimeTinoco7@users.noreply.github.com>
2023-09-30 17:07:10 -0500

    Add files via upload

---
+++ src/laboratorio06/Laboratorio06.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio06;
+
+import interfaces.GestorCursos;
+
+/**
+ *
+ * @author jaime
+ */
+public class Laboratorio06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GestorCursos curso = new GestorCursos();
+        curso.setLocationRelativeTo(null);
+        curso.setVisible(true);
+    }
+
+}
